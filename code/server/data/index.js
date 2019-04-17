30754113312b3dcd365938a7e72d84807a2697b9
Master-Alcy <aijingxuan92@gmail.com>
2019-04-17 19:39:37 -0400

    add

---
+++ code/server/data/index.js
@@ -0,0 +1,9 @@
+const userData = require('./users');
+const blogData = require('./blogs');
+const commentData = require('./comments');
+
+module.exports = {
+    users: userData,
+    blogs: blogData,
+    comments: commentData
+}
