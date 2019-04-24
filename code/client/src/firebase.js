e34eb060039a6a6c047571f6331de0026a0a53a5
hshah42 <43765691+hshah42@users.noreply.github.com>
2019-04-24 19:14:18 -0400

    Update firebase.js

--- code/client/src/firebase.js
+++ code/client/src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyA_sTy2FDJnEEOXJLInfoKJH_MGJCAHpFM",
-    authDomain: "ourblog-3b6f3.firebaseapp.com",
-    databaseURL: "https://ourblog-3b6f3.firebaseio.com",
-    projectId: "ourblog-3b6f3",
-    storageBucket: "ourblog-3b6f3.appspot.com",
-    messagingSenderId: "579554998450"
+    
 };
 
 firebase.initializeApp(config);
