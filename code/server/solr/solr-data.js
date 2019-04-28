67d12a905c8bdf1dc2c986c1e06c4bf37b8b7e28
Hem Shah <hshah42@stevens.edu>
2019-04-27 21:54:02 -0400

    solr functions

---
+++ code/server/solr/solr-data.js
@@ -0,0 +1,98 @@
+let solrClient = require('./solr-config');
+
+var data = {
+    id: "12345678",
+    blog_title: "the TV of a content",
+    blog_text: `So you want to head to Macau but don’t want to spend a small fortune? It’s not just all expensive luxury accommodation and frittering away your hard-earned in the opulent casinos. Truth be told, you can do Macau on a budget – it simply takes a planning.
+
+    Macau is an autonomous territory that is connected to the Chinese mainland. It’s known as the Las Vegas of Asia – a place where people go for gambling and glitz. But it’s much more than that. It has a rich history, stunning architecture, and plenty of waterside vistas to explore.
+    
+    If you’re organized, you can see a lot of Macau in just a day, otherwise, it’s the perfect spot for a long weekend. In Macau, the currency is the Macanese Pataca (MOP), or Hong Kong Dollars (HKD) are also widely accepted as well. Let’s take a look at how you can keep your costs down in this exciting city.`
+};
+
+async function update(document)
+{
+    try
+    {
+        let result = await solrClient.update(document);
+        await solrClient.commit();
+        console.log("success" + JSON.stringify(result));
+
+        if(result.responseHeader.status === 0)
+        {
+            return true;
+        }
+    }
+    catch(e)
+    {
+        console.log("error" + e);
+    }
+
+    return false;
+}
+
+async function createSolrCompatibleDocument(id, title, text)
+{
+    let document = {
+        id: id,
+        blog_title: title,
+        blog_text: text
+    };
+
+    let titleWords = title.split(" ");
+
+    document["blog_title_search"] = titleWords;
+
+    return document;
+}
+
+async function deleteDocument(id)
+{
+    try
+    {
+        let result = await solrClient.delete({ "id": id });
+        await solrClient.commit();
+
+        if(result.responseHeader.status === 0)
+        {
+            return true;
+        }
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
+    
+    return false;
+}
+
+async function queryTitle(text)
+{
+    let documents = [];
+    let query = `blog_title: \"${text}\" OR blog_title:*${text}* OR blog_title_search: \"${text}\" `;
+
+    let searchTermTokens = text.split(" ");
+
+    if(searchTermTokens.length > 1)
+    {
+        for(let i in searchTermTokens)
+        {
+            query = query + `blog_title: \"${searchTermTokens[i]}\" OR blog_title:*${searchTermTokens[i]}* OR blog_title_search: \"${searchTermTokens[i]}\" `;
+        }
+    }
+
+    console.log(query);
+
+    let solrQuery = solrClient.query().q(query);
+
+    let result = await solrClient.search(solrQuery);
+
+    console.log(result.response);
+}
+
+async function main()
+{
+    update(await createSolrCompatibleDocument(data.id, data.blog_title, data.blog_text));
+}
+
+queryTitle("content");
